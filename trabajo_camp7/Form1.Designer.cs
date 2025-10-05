2da3abb4408fae380d7a3e603f12e41b739433b1
josehp660 <115431293+josehp660@users.noreply.github.com>
2025-10-05 11:24:43 -0600

    Agregar archivos de proyecto.

---
+++ trabajo_camp7/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace trabajo_camp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtSalarioMensual = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.lstResultadoCadena = new System.Windows.Forms.ListBox();
+            this.lstSalarioAnual = new System.Windows.Forms.ListBox();
+            this.lstSalarioMensual = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(26, 115);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(144, 27);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(26, 187);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(144, 27);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // txtSalarioMensual
+            // 
+            this.txtSalarioMensual.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalarioMensual.Location = new System.Drawing.Point(26, 285);
+            this.txtSalarioMensual.Name = "txtSalarioMensual";
+            this.txtSalarioMensual.Size = new System.Drawing.Size(144, 27);
+            this.txtSalarioMensual.TabIndex = 2;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(27, 327);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(116, 35);
+            this.btnProcesar.TabIndex = 3;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // lstResultadoCadena
+            // 
+            this.lstResultadoCadena.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstResultadoCadena.FormattingEnabled = true;
+            this.lstResultadoCadena.ItemHeight = 18;
+            this.lstResultadoCadena.Location = new System.Drawing.Point(233, 84);
+            this.lstResultadoCadena.Name = "lstResultadoCadena";
+            this.lstResultadoCadena.Size = new System.Drawing.Size(533, 202);
+            this.lstResultadoCadena.TabIndex = 4;
+            // 
+            // lstSalarioAnual
+            // 
+            this.lstSalarioAnual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstSalarioAnual.FormattingEnabled = true;
+            this.lstSalarioAnual.ItemHeight = 18;
+            this.lstSalarioAnual.Location = new System.Drawing.Point(233, 380);
+            this.lstSalarioAnual.Name = "lstSalarioAnual";
+            this.lstSalarioAnual.Size = new System.Drawing.Size(181, 58);
+            this.lstSalarioAnual.TabIndex = 5;
+            // 
+            // lstSalarioMensual
+            // 
+            this.lstSalarioMensual.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstSalarioMensual.FormattingEnabled = true;
+            this.lstSalarioMensual.ItemHeight = 18;
+            this.lstSalarioMensual.Location = new System.Drawing.Point(233, 304);
+            this.lstSalarioMensual.Name = "lstSalarioMensual";
+            this.lstSalarioMensual.Size = new System.Drawing.Size(280, 58);
+            this.lstSalarioMensual.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(246, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 50);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Mi Sueldo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 22);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nombre :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Apellido :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Salario Mensual :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstSalarioMensual);
+            this.Controls.Add(this.lstSalarioAnual);
+            this.Controls.Add(this.lstResultadoCadena);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.txtSalarioMensual);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtSalarioMensual;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ListBox lstResultadoCadena;
+        private System.Windows.Forms.ListBox lstSalarioAnual;
+        private System.Windows.Forms.ListBox lstSalarioMensual;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ trabajo_camp7/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabajo_camp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            {
+                // ------------------ PREPARACIÓN ------------------
+                lstResultadoCadena.Items.Clear();
+                lstSalarioMensual.Items.Clear();
+                lstSalarioAnual.Items.Clear();
+
+                // Obtener valores de los TextBoxes (cadenas)
+                string nombre = txtNombre.Text;
+                string apellido = txtApellido.Text;
+                string salarioTexto = txtSalarioMensual.Text;
+
+                // 1. Limpieza con Trim() para eliminar espacios innecesarios al inicio y final
+                string nombreLimpio = nombre.Trim();
+                string nombreCompleto = nombreLimpio + " " + apellido.Trim(); // Concatenación
+
+                lstResultadoCadena.Items.Add("--- MANIPULACIÓN DE CADENAS ---");
+                lstResultadoCadena.Items.Add($"Nombre Completo (Concatenado): {nombreCompleto}");
+                lstResultadoCadena.Items.Add($"Longitud del Nombre: {nombreLimpio.Length}");
+
+                // 2. Acceso por índice: Primer carácter (VALIDADO)
+                if (nombreLimpio.Length > 0)
+                {
+                    lstResultadoCadena.Items.Add($"Primer Carácter: {nombreLimpio[0]}");
+                }
+                else
+                {
+                    lstResultadoCadena.Items.Add($"Primer Carácter: (Nombre vacío)");
+                }
+
+                // Conversión a mayúsculas/minúsculas
+                lstResultadoCadena.Items.Add($"Nombre Completo en Mayúsculas: {nombreLimpio.ToUpper()}");
+                ;
+                lstResultadoCadena.Items.Add($"Nombre Completo en Minúsculas: {nombreLimpio.ToLower()}");
+
+                // 3. Substring: Primeros 3 caracteres (VALIDADO)
+                string subStringResultado = nombreLimpio.Length >= 3 ?
+                                            nombreLimpio.Substring(0, 3) :
+                                            "Menos de 3 caracteres";
+                lstResultadoCadena.Items.Add($"Substring (Primeros 3): {subStringResultado}");
+
+                // IndexOf: Posición de la primera 'a'
+                int indiceA = nombreLimpio.ToLower().IndexOf('a');
+                lstResultadoCadena.Items.Add($"Índice de la primera 'a': {indiceA}");
+
+                // Replace: Reemplazar 'a' por '@'
+                string replaceResultado = nombreLimpio.Replace('a', '@').Replace('A', '@');
+                lstResultadoCadena.Items.Add($"Replace ('a' por '@'): {replaceResultado}");
+
+                // Split y Ordenación de palabras
+                lstResultadoCadena.Items.Add("--- Split y Ordenación ---");
+                string[] palabras = nombreCompleto.Split(' ');
+                List<string> palabrasOrdenadas = new List<string>(palabras);
+                palabrasOrdenadas.Sort();
+                lstResultadoCadena.Items.Add("Palabras ordenadas:");
+                foreach (string p in palabrasOrdenadas)
+                {
+                    if (!string.IsNullOrWhiteSpace(p)) // 4. Ignora elementos vacíos
+                    {
+                        lstResultadoCadena.Items.Add($" - {p}");
+                    }
+                }
+
+
+                // ------------------ CÁLCULO DE SALARIO ------------------
+
+                lstSalarioMensual.Items.Add("--- CÁLCULO DE SALARIO ---");
+
+                decimal salarioMensual;
+                // Uso de TryParse para prevenir errores si el salario no es un número
+                if (decimal.TryParse(salarioTexto, out salarioMensual))
+                {
+                    lstSalarioMensual.Items.Add($"Mensual: {salarioMensual:C}");
+
+                    decimal salarioAnual = salarioMensual * 12;
+
+                    lstSalarioAnual.Items.Add($"Anual: {salarioAnual:C}");
+                }
+                else
+                {
+                    // Mensaje de error si la conversión falla
+                    lstSalarioMensual.Items.Add("ERROR: Salario Mensual no válido.");
+                    lstSalarioAnual.Items.Add("ERROR: Verifique el ingreso.");
+                }
+            }
+        }
+        
+    }
+}
